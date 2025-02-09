ea2981958722ca1948d881bb080f0a4af63407b1
HarryTheClem <harry.clements@btinternet.com>
2025-02-09 15:10:20 +0000

    Added dataset with additional columns
    Added an extra dataset with more feaures to pipeline for more comprehensive analysis and removed identifiable file paths

--- src/dataProcessingPipeline.py
+++ src/dataProcessingPipeline.py
@@ -55,12 +55,47 @@ team_pivot = slim_elements_df.pivot_table(index = 'team', values = 'value', aggf
 
 
 
-slim_elements_df.to_csv('/Users/harryclements/Documents/GitHub/FPL-Dashboard/players_data.csv')
+slim_elements_df.to_csv('./players_data.csv')
 
 
 
+# Second more comprehensive dataframe to use
 
 
+# Get teams
+teams_df = pd.DataFrame(json['teams'])
+# Get players
+elements_df = pd.DataFrame(json['elements'])
+# Get links to positions etc.
+element_stats_df = pd.DataFrame(json['element_stats'])
+element_types_df = pd.DataFrame(json['element_types'])
+
+# Combine all of the dataframes to get the positions and teams
+combined_team_df = elements_df.merge(teams_df, left_on = "team", right_on = "id", how = "left")
+combined_team_position_df = combined_team_df.merge(element_types_df, left_on = "element_type", right_on = "id")
+
+# Have a quick look at the shape of the dataframe
+combined_team_position_df.shape
+
+# Filter out players who don't have any points
+players_with_points = combined_team_position_df[combined_team_position_df["total_points"] != 0]
+
+
+
+columns_for_investigation = ["web_name", "name", "singular_name", "now_cost", "total_points", "influence", "creativity", "threat", "ict_index",
+                             "clean_sheets", "expected_goals", "expected_assists", "expected_goal_involvements",
+                               "expected_goals_conceded", "expected_goals_per_90", "expected_assists_per_90",
+                                 "expected_goal_involvements_per_90", "expected_goals_conceded_per_90", "goals_conceded_per_90",
+                                 "starts_per_90", "clean_sheets_per_90"]
+
+
+plotting_data = players_with_points.loc[:, columns_for_investigation]
+
+for i in columns_for_investigation[3:]:
+    plotting_data[i] = plotting_data[i].astype(float)
+
+
+plotting_data.to_csv("./comprehensive_players_data.csv")
 
 
 
